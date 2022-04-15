6ec41886fb4ef390a5bf0e282aa2777c1ce84481
welly091 <whtp1490@gmail.com>
2022-04-15 14:22:44 -0700

    Add more books

--- src/js/booklist.js
+++ src/js/booklist.js
@@ -16,7 +16,18 @@ let booklist = [
     {id:14, price: 1.98, title:'Who Fears Death',src:'src/img/who_fears_death.jpg'},
     {id:15, price: 1.64, title:'Song of Blood and Stone', src:'src/img/song_of_blood_and_stone.jpg'},
     {id:16, price:2.02, title:'We Hunt the Flame', src:'src/img/we_hunt_the_flame.jpg'},
-    {id:17, price: 2.89, title:'Harry Potter and the Order of the Phoenix', src:'src/img/harry_potter_and_the_order_of_the_phoenix.jpg'}
+    {id:17, price: 2.89, title:'Harry Potter and the Order of the Phoenix', src:'src/img/harry_potter_and_the_order_of_the_phoenix.jpg'},
+    {id:18, price: 2.55, title:'Educated: A Memoir', src:'src/img/educated_a_memoir.jpg'},
+    {id:19, price: 3.80, title:'Little Fires Everywhere', src:'src/img/little_fires_everywhere.jpg'},
+    {id:20, price: 1.98, title:'The Outsider', src:'src/img/the_outsider.jpg'},
+    {id:21, price: 2.21, title:'Becoming', src:'src/img/becoming.jpg'},
+    {id:22, price:2.15, title:'All the Light We Cannot See', src:'src/img/all_the_light_we_cannot_see.jpg'},
+    {id:23, price:1.99, title:'Where the Crawdads Sing', src:'src/img/where_the_crawdads_sing.jpg'},
+    {id:24, price:1.57, title:'Midnight Sun', src:'src/img/midnight_sun.jpg'},
+    {id:25, price:1.49, title:'The Deep End', src:'src/img/the_deep_end.jpg'},
+    {id:26, price:2.19, title:'The Fault in Our Stars', src:'src/img/the_fault_in_our_starts.jpg'},
+    {id:27, price:2.01, title:'The House Girl', src:'src/img/the_house_girl.jpg'},
+    {id:28, price:2.86, title:'Wonder', src:'src/img/wonder.jpg'}
 ]
 
 //Create a shopping cart list in sessionStorage
